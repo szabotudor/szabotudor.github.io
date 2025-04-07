7efb873c4b3b18132fcfe7c2c63be7b7cb29aa20
szabo-tudor <szabo.tudor@gmail.com>
2025-04-07 14:56:33 +0200

    Added The Final Print

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("lua-experience").textContent = calculateExperience(2016, 2018);
     document.getElementById("cpp-experience").textContent = calculateExperience(2017);
     document.getElementById("godot-experience").textContent = calculateExperience(2019);
-    document.getElementById("unreal-experience").textContent = calculateExperience(2021);
+    document.getElementById("unreal-experience").textContent = calculateExperience(2022);
 });
